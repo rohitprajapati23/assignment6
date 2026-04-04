daddc92a6117d2aced8d190a8660e72f6ff3e856
Rohit Prajapati <rohitprajapati232224@gmail.com>
2026-04-04 11:32:55 +0530

    Initial commit

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const server = http.createServer((req, res) => {
 
 
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
