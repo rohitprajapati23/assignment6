364932195ef7ff7f5ad5e3ce20c072ea19a41805
Rohit Prajapati <rohitprajapati232224@gmail.com>
2026-09-12 23:39:25 +0530

    Fix typo in server log message

--- server.js
+++ server.js
@@ -13,5 +13,5 @@ const server = http.createServer((req, res) => {
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
-    console.log(`Serversh running at http://localhost:${PORT}`);
+    console.log(`Servers running at http://localhost:${PORT}`);
 });
